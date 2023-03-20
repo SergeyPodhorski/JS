630399153a0107795535f461fdeff89deab4f2b3
SergeyPodhorski <117593285+SergeyPodhorski@users.noreply.github.com>
2023-03-21 00:26:25 +0300

    Delete string directory

--- string/task-08.js
+++
@@ -1,9 +0,0 @@
-// 8)Создать строку: 10 times 2 is 20' используя переменные:
-// const a = 10;
-// const b = 2;
-// Используйте шаблонные строки.
-
-const a = 10;
-const b = 2;
-let str8 = `${a} times ${b} is 20`;
-console.log(str8);
