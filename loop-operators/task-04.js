19306ab95222b476630f9e73a38cb13622bf7217
SergeyPodhorski <117593285+SergeyPodhorski@users.noreply.github.com>
2023-03-22 21:44:47 +0300

    Add files via upload

---
+++ loop-operators/task-04.js
@@ -0,0 +1,12 @@
+// 4)Нарисуйте в консоле пирамиду на 10 уровней как показано ниже
+//         x
+//        xxx
+//       xxxxx
+//      xxxxxxx
+//     xxxxxxxxx
+
+let str = "";
+for (let i = 0; i <=10; i++) {
+    str += "х";
+    console.log(str + " ");
+}
