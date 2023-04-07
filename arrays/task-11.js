c738687201171127d8b9b8eaf9229fc7fae833c6
SergeyPodhorski <117593285+SergeyPodhorski@users.noreply.github.com>
2023-04-07 14:00:34 +0300

    Delete arrays directory

--- arrays/task-11.js
+++
@@ -1,5 +0,0 @@
-// 11)Создать массив любых элементов. Получить случайный элемент из массива и 
-// вывести его в консоль.
-let country = [`Russia`, `USA`, `Germany`, `Poland`, `Ukraine`];
-let randomCountry = Math.floor(Math.random() * country.length);
-console.log(country[randomCountry]);
